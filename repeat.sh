f6d525a0a5e719d4ad0dd3868f932e458b8aedd4
Gasper Kojek <gkojek@gradle.com>
2025-01-21 20:03:54 +0100

    Fixed gha tty

--- repeat.sh
+++ repeat.sh
@@ -20,12 +20,15 @@ for i in $(seq 1 $iterations); do
   # Execute the command and capture output
   # output=$(eval "$command")
   # Execute the command, capture output, and display it in real-time
-  output=$(eval "$command" | tee /dev/tty | grep "\[INFO] Total time:" | sed -E 's/.*:\s+([0-9.:]+)\s+(s|min).*$/\1/')
+#  output=$(eval "$command" | tee /dev/tty | grep "\[INFO] Total time:" | sed -E 's/.*:\s+([0-9.:]+)\s+(s|min).*$/\1/')
+  eval "$command" | tee output.txt
+  output=$(cat output.txt | grep "\[INFO] Total time:" | sed -E 's/.*:\s+([0-9.:]+)\s+(s|min).*$/\1/')
+  rm output.txt
 
   time=$(echo "$output" | grep "\[INFO] Total time:")
 
   # Convert minutes to seconds if necessary
-  if $(echo "$time" | grep -q "min"); then
+  if echo "$time" | grep -q "min"; then
     time=$(echo "$time" | sed -E 's/.*: +([0-9:]+) +min.*/\1/')
     minutes=$(echo "$time" | cut -d: -f1 | bc)
     seconds=$(echo "$time" | cut -d: -f2 | bc)
